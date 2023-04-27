b44c431ccbd1810dd2394cc6f8480672eb96c0eb
Rafał Gałecki <darthunter@gmail.com>
2023-04-27 16:58:46 +0200

    optimized

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,9 +1,41 @@
-export const getTasks = state => state.tasks.items;
+import { createSelector } from '@reduxjs/toolkit';
+import { statusFilters } from './constants';
+
+export const selectTasks = state => state.tasks.items;
 // items added
-export const getStatusFilter = state => state.filters.status;
+export const selectStatusFilter = state => state.filters.status;
 
 // The selectors for api ops:
 
-export const getIsLoading = state => state.tasks.isLoading;
+export const selectIsLoading = state => state.tasks.isLoading;
 
-export const getError = state => state.tasks.error;
+export const selectError = state => state.tasks.error;
+
+export const selectVisibleTasks = createSelector(
+  [selectTasks, selectStatusFilter],
+  (tasks, statusFilter) => {
+    switch (statusFilter) {
+      case statusFilters.active:
+        return tasks.filter(task => !task.completed);
+      case statusFilters.completed:
+        return tasks.filter(task => task.completed);
+      default:
+        return tasks;
+    }
+  }
+);
+
+//createSelector to limit unnecessary rendering
+export const selectTaskCount = createSelector([selectTasks], tasks => {
+  return tasks.reduce(
+    (acc, task) => {
+      if (task.completed) {
+        acc.completed += 1;
+      } else {
+        acc.active += 1;
+      }
+      return acc;
+    },
+    { active: 0, completed: 0 }
+  );
+});
--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -10,6 +10,15 @@ const handleRejected = (state, action) => {
   state.error = action.payload;
 };
 
+// The two functions beneath are for builder:
+const isPendingAction = action => {
+  return action.type.endsWith('/pending');
+};
+
+const isRejectAction = action => {
+  return action.type.endsWith('/rejected');
+};
+
 const tasksSlice = createSlice({
   name: 'tasks',
   initialState: {
@@ -17,41 +26,83 @@ const tasksSlice = createSlice({
     isLoading: false,
     error: null,
   },
-  extraReducers: {
-    [fetchTasks.pending]: handlePending,
-    [fetchTasks.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-    [fetchTasks.rejected]: handleRejected,
-    [addTask.pending]: handlePending,
-    [addTask.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items.push(action.payload);
-    },
-    [addTask.rejected]: handleRejected,
-    [deleteTask.pending]: handlePending,
-    [deleteTask.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      const index = state.items.findIndex(
-        task => task.id === action.payload.id
-      );
-      state.items.splice(index, 1);
-    },
-    [deleteTask.rejected]: handleRejected,
-    [toggleCompleted.pending]: handlePending,
-    [toggleCompleted.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      const index = state.items.findIndex(
-        task => task.id === action.payload.id
-      );
-      state.items.splice(index, 1, action.payload);
-    },
-    [toggleCompleted.rejected]: handleRejected,
+  // extraReducers: {
+  //   [fetchTasks.pending]: handlePending,
+  //   [fetchTasks.fulfilled](state, action) {
+  //     state.isLoading = false;
+  //     state.error = null;
+  //     state.items = action.payload;
+  //   },
+  //   [fetchTasks.rejected]: handleRejected,
+  //   [addTask.pending]: handlePending,
+  //   [addTask.fulfilled](state, action) {
+  //     state.isLoading = false;
+  //     state.error = null;
+  //     state.items.push(action.payload);
+  //   },
+  //   [addTask.rejected]: handleRejected,
+  //   [deleteTask.pending]: handlePending,
+  //   [deleteTask.fulfilled](state, action) {
+  //     state.isLoading = false;
+  //     state.error = null;
+  //     const index = state.items.findIndex(
+  //       task => task.id === action.payload.id
+  //     );
+  //     state.items.splice(index, 1);
+  //   },
+  //   [deleteTask.rejected]: handleRejected,
+  //   [toggleCompleted.pending]: handlePending,
+  //   [toggleCompleted.fulfilled](state, action) {
+  //     state.isLoading = false;
+  //     state.error = null;
+  //     const index = state.items.findIndex(
+  //       task => task.id === action.payload.id
+  //     );
+  //     state.items.splice(index, 1, action.payload);
+  //   },
+  //   [toggleCompleted.rejected]: handleRejected,
+  // },
+
+  //beacuse of extraReducers are deprecated, we use builder callback:
+  extraReducers: builder => {
+    builder
+      // .addCase(fetchTasks.pending, handlePending)
+      // .addCase(addTask.pending, handlePending)
+      // .addCase(toggleCompleted.pending, handlePending)
+      // .addCase(fetchTasks.rejected, handleRejected)
+      // .addCase(addTask.rejected, handleRejected)
+      // .addCase(toggleCompleted.rejected, handleRejected)
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(addTask.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items.push(action.payload);
+      })
+      .addCase(deleteTask.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        const index = state.items.findIndex(
+          task => task.id === action.payload.id
+        );
+        state.items.splice(index, 1);
+      })
+      .addCase(toggleCompleted.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        const index = state.items.findIndex(
+          task => task.id === action.payload.id
+        );
+        state.items[index].completed = !state.items[index].completed;
+      })
+      .addMatcher(isPendingAction, handlePending)
+      .addMatcher(isRejectAction, handleRejected)
+      .addDefaultCase((state, action) => {
+        state.error = 'someone use old function, fix it!';
+      });
   },
 });
 
